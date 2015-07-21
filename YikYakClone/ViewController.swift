94274965b04d3c3839f7bc9b3e2181d6e5002ec2
Dane <dane.e.jordan@gmail.com>
2015-07-21 18:08:07 -0500

    Initial Commit

---
+++ YikYakClone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YikYakClone
+//
+//  Created by Dane Jordan on 7/21/15.
+//  Copyright (c) 2015 Dane Jordan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
